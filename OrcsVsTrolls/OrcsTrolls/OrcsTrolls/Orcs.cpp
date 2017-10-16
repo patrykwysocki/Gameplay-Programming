8b659b8134829226dd79ac6b35eb9e3cdca04430
patrykwysocki <c00213855@itcarlow.ie>
2017-10-16 22:38:06 +0100

    commit project

---
+++ OrcsVsTrolls/OrcsTrolls/OrcsTrolls/Orcs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Trolls.h"
+#include "Orcs.h"
+#include "Characters.h"
+//contructor for orc
+Orcs::Orcs()
+{
+	mana = 100;
+	health = 100;
+	energy=100;
+	battle = true;
+
+}
